5039a3a159a656b7aeca58f51fef63a78132c2e5
ditinagrawal <ditin.agrawal05@gmail.com>
2024-12-31 15:14:26 +0530

    Two Pointers

---
+++ Arrays&Strings/Theory/TwoPointer.java
@@ -0,0 +1,17 @@
+public class TwoPointer {
+    public static void main(String[] args) {
+        System.out.println("Two Pointer Technique");
+
+        /*
+         * It involves using two pointers (usually represented as indices) to traverse a
+         * collection, often in a linear manner.
+         * 
+         * ======= Key Concepts:
+         * 1. Two Pointers: we maintain two pointers. These pointers move based on the
+         * problem's conditions.
+         * 
+         * 2.Increment and Decrement: The pointers typically move in a systematic way
+         * 
+         */
+    }
+}
